2865487b0bb4e4bcfe925b8ac91ede9e69521efe
gqkc <guillaume.quispe@gmail.com>
2021-10-29 16:00:56 +0200

    train

--- labml_nn/diffusion/ddpm/experiment.py
+++ labml_nn/diffusion/ddpm/experiment.py
@@ -150,7 +150,7 @@ class Configs(BaseConfigs):
         """
         for _ in monit.loop(self.epochs):
             # Train the model
-            # self.train()
+            self.train()
             # Sample some images
             self.sample()
             # New line in the console
