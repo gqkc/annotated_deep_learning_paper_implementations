411db15941ccef8800c3770711624036fd18f164
gqkc <guillaume.quispe@gmail.com>
2021-10-30 20:35:22 +0200

    cpu

--- labml_nn/diffusion/ddpm/ddpm_utils.py
+++ labml_nn/diffusion/ddpm/ddpm_utils.py
@@ -12,7 +12,7 @@ import torch.utils.data
 
 def gather(consts: torch.Tensor, t: torch.Tensor):
     """Gather consts for $t$ and reshape to feature map shape"""
-    c = consts.gather(-1, t)
+    c = consts.to(t.device).gather(-1, t)
     return c.reshape(-1, 1, 1, 1)
 
 
