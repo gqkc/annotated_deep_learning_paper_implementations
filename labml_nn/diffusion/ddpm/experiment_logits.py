a391ab5fe2b76d2827d9fcd0babac7ec2b3bb840
gqkc <guillaume.quispe@gmail.com>
2021-11-25 14:04:52 +0100

    nsteps 1000

--- labml_nn/diffusion/ddpm/experiment_logits.py
+++ labml_nn/diffusion/ddpm/experiment_logits.py
@@ -354,7 +354,7 @@ def get_parser():
     parser.add_argument('--vq_path', type=str)
     parser.add_argument('--train_dataset_path', type=str)
     parser.add_argument('--kl', type=bool, default=False)
-    parser.add_argument('--n_steps', type=int, default=200)
+    parser.add_argument('--n_steps', type=int, default=1000)
     parser.add_argument('--transform', type=str, default="permute")
 
     parser.add_argument("--latent_dim", default=32, type=int)
