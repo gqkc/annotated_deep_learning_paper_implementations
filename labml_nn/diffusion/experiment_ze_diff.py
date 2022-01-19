2c9b778a179a84e9cc3959836b0ad8225b6952b9
gqkc <guillaume.quispe@gmail.com>
2022-01-19 11:59:13 +0100

    dataset from image miniimagenet

--- labml_nn/diffusion/experiment_ze_diff.py
+++ labml_nn/diffusion/experiment_ze_diff.py
@@ -277,7 +277,7 @@ def mini128_dataset(c: Configs):
     """
     Create mini imagenet dataset with 128 pixels
     """
-    transform = transforms.Compose([transforms.Resize((32, 32)),
+    transform = transforms.Compose([transforms.Resize((128, 128)),
                                     transforms.ToTensor(),
                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
                                     ])
